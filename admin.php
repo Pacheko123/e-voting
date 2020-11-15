74e7b20d115b384ca26ddb5b81cd9238a846ab5f
root <root@localhost.localdomain>
2020-11-15 11:35:40 +0300

    modified admin.php

--- admin.php
+++ admin.php
@@ -70,3 +70,16 @@
 <div class="text-right">
 <button type="button" class="btn btn-dark">SUBMIT</button>
 </div>
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+<!-- Your custom scripts (optional) -->
+<script type="text/javascript">// Animations init
+  new WOW().init();
+</script>
